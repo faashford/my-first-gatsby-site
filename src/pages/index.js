b8b9a99eb516caff01ca017b3ae500957ee8baa4
faashford <39807568+faashford@users.noreply.github.com>
2021-12-08 18:02:43 -0800

    Finished Gatsby Tutorial 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
 	return (
         <Layout pageTitle="Home Page">
 			<p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage
+                alt="A human holding on to a flying saucer that is taking off, leaving Earth"
+                src="../images/EscapeFromEarth.jpg"
+            />
 		</Layout>
 	)
 }
